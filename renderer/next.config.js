9650bea136756a00801e87a3640c6df294b36eb4
Arlo Liu <arlo@xfers.com>
2021-11-20 20:24:35 +0800

    rebuild

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,4 +1,7 @@
+const basePath = '/greatday-clock-in';
 module.exports = {
+  basePath: basePath,
+  assetPrefix: `${basePath}/`,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       if (!process.env.BUILD_WEB) {
