ce9409999de7176524ce79028debcf73c731140d
Arlo Liu <arlo@xfers.com>
2021-11-20 21:00:58 +0800

    fix asset path

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,7 +1,10 @@
-const basePath = '/greatday-clock-in';
+const basePath = process.env.BUILD_WEB ? '/greatday-clock-in' : '';
 module.exports = {
   basePath: basePath,
   assetPrefix: `${basePath}/`,
+  env: {
+    BACKEND_URL: basePath,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       if (!process.env.BUILD_WEB) {
--- renderer/pages/_app.jsx
+++ renderer/pages/_app.jsx
@@ -29,9 +29,9 @@ export default function app(props) {
     <React.Fragment>
       <Head>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href={`${process.env.BACKEND_URL}/apple-touch-icon.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`${process.env.BACKEND_URL}/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`${process.env.BACKEND_URL}/favicon-16x16.png`} />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <ThemeProvider theme={theme}>
